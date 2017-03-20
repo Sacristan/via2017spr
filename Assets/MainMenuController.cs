47b9fffb5c2be6a3fbe86ada31166e554974601e
Sacristan <girts.kesteris@gmail.com>
2017-03-20 12:31:27 +0200

    Added Main menu

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void Play(){
+		Debug.Log ("Play pressed");
+		SceneManager.LoadScene (1);
+	}
+
+	public void Quit(){
+		Debug.Log ("Quit pressed");
+		Application.Quit ();
+	}
+
+}
