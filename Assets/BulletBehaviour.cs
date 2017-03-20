d8161c002382fb4e4dbdd367ff7118e32b83c8ae
Sacristan <girts.kesteris@gmail.com>
2017-03-20 13:45:03 +0200

    Added drunken movement

---
+++ Assets/BulletBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour {
+	[SerializeField]
+	private float speed = 5f;
+
+	void Update () {
+		transform.position += transform.forward * Time.deltaTime * speed;
+	}
+}
---
+++ Assets/CharacterBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterBehavior : MonoBehaviour {
+
+	private CharacterController characterController;
+		
+	[SerializeField]
+	private float speed = 5f;
+
+	[SerializeField]
+	private float horizontalRotationSpeed = 200f;
+	[SerializeField]
+	private float verticalRotationSpeed = 200f;
+
+	[SerializeField]
+	private GameObject bulletPrefab;
+
+	[SerializeField]
+	private Transform bulletSpawnTransform;
+
+	void Start () {
+		characterController = GetComponent<CharacterController> ();
+	}
+	
+	void Update () {
+		float horizontalInput = Input.GetAxis ("Horizontal");	
+		float verticalInput = Input.GetAxis ("Vertical");
+
+		float h = horizontalRotationSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
+		float v = verticalRotationSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime;
+	
+		Vector3 inputForMovement = new Vector3 (horizontalInput, 0, verticalInput);
+
+		Vector3 deltaInput = inputForMovement * Time.deltaTime * speed;
+		deltaInput.y = Physics.gravity.y;
+
+		transform.Rotate(-v, h, 0);
+		characterController.Move (deltaInput);
+
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			Instantiate (bulletPrefab, bulletSpawnTransform.position, Quaternion.identity);
+		}
+	}
+}
